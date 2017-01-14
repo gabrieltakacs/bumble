76f04d71d231e1093be2546be79823ad14a3e2da
Matúš Demko <demko.mat@gmail.com>
2017-01-14 18:07:28 +0200

    Allow some fields to be non-editable

--- resources/views/fieldTypes/HasOneField.blade.php
+++ resources/views/fieldTypes/HasOneField.blade.php
@@ -2,30 +2,40 @@
     <div class="g-col-2 tar">
         {!! BumbleForm::label($field->getColumn(), $field->getRelatedTitle(), ['class' => 'label label--tar'.$model->getRequiredClass($field)]) !!}
     </div>
-     <div class="g-col-10">
+    <div class="g-col-10">
         <?php
-            $values = [];
+        $values = [];
 
-            if ( ! $model->fieldIsRequired($field)) $values[0] = '--';
+        if ( ! $model->fieldIsRequired($field)) $values[0] = '--';
 
-            $options = $model->{$field->method()}()->getRelated()->lists($field->getRelatedTitleColumn(), 'id');
+        $options = $model->{$field->method()}()->getRelated()->lists($field->getRelatedTitleColumn(), 'id');
 
-            foreach ($options as $key => $value)
-            {
-                $values[$key] = $value;
-            }
+        foreach ($options as $key => $value)
+        {
+            $values[$key] = $value;
+        }
 
-            if ($field->hidesSelf())
-            {
-                // This entries ID
-                $removeId = \BumbleForm::getValueAttribute('id');
+        if ($field->hidesSelf())
+        {
+            // This entries ID
+            $removeId = \BumbleForm::getValueAttribute('id');
 
-                // Remove this entry from the values
-                if (isset($removeId)) unset($values[$removeId]);
-            }
+            // Remove this entry from the values
+            if (isset($removeId)) unset($values[$removeId]);
+        }
         ?>
-
-            {!! BumbleForm::select($field->getColumn(), $values, null, ['class' => 'input input1', 'placeholder' => $field->getPlaceholder()]) !!}
+        <?php
+        $editable = true;
+        if(key_exists('editable', $field->getOptions())){
+            $editable = $field->getOptions()['editable'];
+        }
+        ?>
+        <?php if ($editable):?>
+        {!! BumbleForm::select($field->getColumn(), $values, null, ['class' => 'input input1', 'placeholder' => $field->getPlaceholder()]) !!}
+        <?php else:?>
+        {!! BumbleForm::hidden($field->getColumn(),  $post->getAttributes()[$field->getColumn()], null, ['class' => 'input input1', 'placeholder' => $field->getPlaceholder()]) !!}
+        {!! BumbleForm::text($field->getColumn() . '_visualisation', $values[$post->getAttributes()[$field->getColumn()]], ['class' => 'input input1', 'placeholder' => $field->getPlaceholder(), 'disabled'])   !!}
+        <?php endif; ?>
     </div>
 </div>
 <?php /*
--- resources/views/fieldTypes/TextField.blade.php
+++ resources/views/fieldTypes/TextField.blade.php
@@ -2,7 +2,19 @@
     <div class="g-col-2 tar">
         {!! BumbleForm::label($field->getColumn(), $field->getTitle(), ['class' => 'label label--tar'.$model->getRequiredClass($field)]) !!}
     </div>
+    <?php
+    $editable = true;
+    if(key_exists('editable', $field->getOptions())){
+        $editable = $field->getOptions()['editable'];
+    }
+    ?>
     <div class="g-col-10">
-        {!! BumbleForm::text($field->getColumn(), $field->getDefaultValue($editing) ?: null, ['class' => 'input input1', 'placeholder' => $field->getPlaceholder()]) !!}
+
+        <?php if ($editable):?>
+        {!! BumbleForm::text($field->getColumn(), $field->getDefaultValue($editing) ?: null, ['class' => 'input input1 ', 'placeholder' => $field->getPlaceholder()]) !!}
+        <?php else:?>
+
+        {!! BumbleForm::text($field->getColumn(), $field->getDefaultValue($editing) ?: null, ['class' => 'input input1 ', 'placeholder' => $field->getPlaceholder(), 'disabled']) !!}
+        <?php endif; ?>
     </div>
 </div>
